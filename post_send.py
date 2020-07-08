8070c02c07c0d84d3f43a7656d8e305615274898
Roman4iks <romaoleg50@gmail.com>
2020-07-08 13:17:23 +0300

    Session + FIX

--- post_send.py
+++ post_send.py
@@ -67,6 +67,7 @@ class Programm():
 
 		self.button_test = tk.Button(root, text = "ТестКнопка", command =self.test)
 		
+		self.list_box = tk.Listbox()
 		self.list_box_2 = tk.Listbox()
 
 		self.L0.grid(row = 0, column = 2)
@@ -88,13 +89,52 @@ class Programm():
 		self.button_files_dir.grid(row = 3, column = 3)
 		self.button_files_add.grid(row = 4, column = 3)
 	
+		self.list_box.grid(row = 4 ,column = 2, sticky = tk.W+tk.E)
 		self.list_box_2.grid(row = 5 ,column = 2, sticky = tk.W+tk.E)
 
 		self.dirs.insert(0,'Выберите файл')
 		self.url.insert(0,'http://httpbin.org/post')
 		
 		self.list_box_2.bind("<<ListboxSelect>>", self.focusbox)
-		
+		root.protocol("WM_DELETE_WINDOW", self.save_session)
+
+		json_data_str = open("session.json")
+		try:
+			json_data = json.load(json_data_str)
+			for data_json in json_data:
+				if json_data[data_json] == None or json_data[data_json] == []:
+					pass
+				else:
+					if data_json == "url":
+						url_data = json_data[data_json]
+						self.url.delete(0, tk.END)
+						self.url.insert(0, url_data)
+					elif data_json == "dirs":
+						dirs_data = json_data[data_json]
+						self.dirs.delete(0, tk.END)
+						self.dirs.insert(0, dirs_data)
+					elif data_json == "name":
+						name_data =json_data[data_json]
+						self.name.delete(0, tk.END)
+						self.name.insert(0, name_data)
+					elif data_json == "data":
+						data_data = json_data[data_json]
+						self.data.delete(0, tk.END)
+						self.data.insert(0, data_data)
+					elif data_json == "data_value":
+						data_value_data = json_data[data_json]
+						self.data_value.delete(0, tk.END)
+						self.data_value.insert(0, data_value_data)
+					elif data_json == "listbox":
+						self.list_box.delete(0, tk.END)
+						for data_list in json_data[data_json]:
+							self.list_box.insert(0, data_list)
+					elif data_json == "listbox2":
+						self.list_box_2.delete(0, tk.END)
+						for data_list in json_data[data_json]:
+							self.list_box_2.insert(0, data_list)
+		except json.decoder.JSONDecodeError:
+			pass
 	def focusbox(self,get):
 		cursor = self.list_box_2.curselection()
 		data_all = self.list_box_2.get(cursor, cursor)
@@ -112,8 +152,11 @@ class Programm():
 				self.dirs.insert(0, data_dir)
 				self.name.insert(0, data_name)
 			else:
+				self.data.delete(0, tk.END)
+				self.data_value.delete(0, tk.END)
+
 				self.data.insert(0, data_key)
-				self.data_valeu.insert(0, data_value)
+				self.data_value.insert(0, data_value)
 
 
 	def file_dir(self):
@@ -180,7 +223,7 @@ class Programm():
 
 	def data_edit(self):
 		index = self.list_box_2.curselection()
-		data = self.list_box_2.get(index, index)[0]
+		data = self.list_box_2.get(index, index)
 		key = data.split(' : ')[0]
 
 		if key == "file":
@@ -248,20 +291,16 @@ class Programm():
 			self.L1['text'] = "Ошибка POST запроса ответа некоректный"
 			return 
 
-		list_box = tk.Listbox()
-		
-		list_box.grid(row = 4 ,column = 2, sticky = tk.W+tk.E)
-
 		for name in json_data:
 			if name == "headers":
 				for name_head in json_data['headers']:
-					list_box.insert(0, f"    {name_head} --> {json_data['headers'][name_head]}")
-				list_box.insert(0, f"{name} -->")
+					self.list_box.insert(0, f"    {name_head} --> {json_data['headers'][name_head]}")
+				self.list_box.insert(0, f"{name} -->")
 			else:
 				if json_data[name] == None or json_data[name] == {} or json_data[name] == " ":
 					pass
 				else:
-					list_box.insert(0, f"{name} --> {json_data[name]}")
+					self.list_box.insert(0, f"{name} --> {json_data[name]}")
 
 	def send_GET(self):
 		url = self.url.get()		
@@ -307,16 +346,13 @@ class Programm():
 			self.L1['text'] = "Ошибка GET запроса ответа некоректный"
 			return 
 
-		list_box = tk.Listbox()
-		list_box.grid(row = 4 ,column = 2, sticky = tk.W+tk.E)
-
 		for key in json_data:
 			if key == "headers":
 				for headers_data in json_data[key]:
-					list_box.insert(0, f"    {headers_data} --> {json_data[key][headers_data]}")
-				list_box.insert(0, f"{key} --> ")
+					self.list_box.insert(0, f"    {headers_data} --> {json_data[key][headers_data]}")
+				self.list_box.insert(0, f"{key} --> ")
 			else:
-				list_box.insert(0, f"{key} --> {json_data[key]}")
+				self.list_box.insert(0, f"{key} --> {json_data[key]}")
 
 	def test(self):
 		q = pyperclip.paste()
@@ -334,6 +370,48 @@ class Programm():
 
 		self.data.insert(0, data_key)
 		self.data_value.insert(0, data_value)
+
+	def save_session(self):
+		url = self.url.get()
+		dirs = self.dirs.get()
+		name = self.name.get()
+		data = self.data.get()
+		data_value = self.data_value.get()
+		data_listbox1 = self.list_box.get(0, tk.END)
+		data_listbox2 = self.list_box_2.get(0, tk.END)
+			
+
+		if data == "":
+			data = None
+
+		if data_value == "":
+			data_value = None
+
+		if name == "":
+			name = None
+
+		if dirs == "" or dirs == "Выберите файл":
+			dirs = None
+
+		if url == "":
+			url = None
+
+		json_data = {}
+		json_data["url"] = url
+		json_data["dirs"] = dirs
+		json_data["name"] = name
+		json_data["data"] = data
+		json_data["data_value"] = data_value
+		json_data["listbox"] = data_listbox1
+		json_data["listbox2"] = data_listbox2
+
+		file = open('session.json', 'w')
+
+		file.write(json.dumps(json_data))
+
+		file.close()
+
+		root.destroy()
 s = Programm(root)
 
 root.mainloop()
